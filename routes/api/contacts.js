6e2e8c8b74b4ebd0a68019d333887350849630de
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-14 10:33:39 +0300

    helpers/error made, router.get ready

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const Joi = require("joi");
+
+const contacts = require("../../models/contacts");
+
+const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
+const addSchema = Joi.object({
+  name: Joi.string().required,
+  email: Joi.string().required,
+  phone: Joi.string().required,
+});
+
 router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const result = await contacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
