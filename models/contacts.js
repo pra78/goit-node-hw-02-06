eb7ceb3a349fd2b484e7505c5f35a8f11ca82acf
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-21 09:56:09 +0300

    error messaages

--- models/contacts.js
+++ models/contacts.js
@@ -32,7 +32,9 @@ const addSchema = Joi.object().keys({
 });
 
 const updateFavouriteSchema = Joi.object({
-  favorite: Joi.boolean().required(),
+  favorite: Joi.boolean().required().messages({
+    "any.required": "missing field favorite",
+  }),
 });
 
 const schemas = {
