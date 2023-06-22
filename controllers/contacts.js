f8721ad99341bb7bf9eecdbb832aa6a2a831c036
pra78 <66877961+pra78@users.noreply.github.com>
2023-06-22 10:12:42 +0300

    pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,11 +4,13 @@ const { HttpError } = require("../helpers");
 
 const listContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
   try {
-    const result = await Contact.find(
-      { owner },
-      "-createdAt -updatedAt"
-    ).populate("owner", "email");
+    const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+      skip,
+      limit,
+    }).populate("owner", "email");
     res.json(result);
   } catch (error) {
     next(error);
